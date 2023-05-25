7b75efdc6e5a7ac6dd843196008442708b44730f
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2023-05-25 14:45:33 -0300

    Criando lista de palavras e gamestate

--- INFO2M-Miguel_William-Caua-Melquisedeque.py
+++ INFO2M-Miguel_William-Caua-Melquisedeque.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 
 SCREENSIZE = (600,600)
 pg.init()
@@ -6,9 +7,27 @@ screen = pg.display.set_mode(SCREENSIZE)
 pg.display.set_caption('Forca')
 clock = pg.time.Clock()
 running = True
+GAMESTATE = 'jogo'
+# jogo
+# gameover
 
-teste = pg.image.load('nao sei.png')
-teste.set_colorkey('white')
+img_coracao = pg.image.load('coracao.png')
+img_coracao = pg.transform.scale(img_coracao, (40,40))
+
+palavras = ['banana','palmeiras','cuscuz','teclado','cachorro','vaca', 'naruto','cleiton','emidio','dias','macaco','computador']
+
+palavra_escolhida = random.choice(palavras)
+resposta = ''
+for i in range(len(palavra_escolhida)):
+    resposta += '_'
+
+def novo_jogo():
+    global palavra_escolhida, resposta
+    palavra_escolhida = random.choice(palavras)
+    
+    resposta = ''
+    for i in range(len(palavra_escolhida)):
+        resposta += '_'
 
 def mostrar_texto(texto,tamanho,pos):
     font = pg.font.Font('Minecraft.ttf',tamanho)
@@ -17,19 +36,40 @@ def mostrar_texto(texto,tamanho,pos):
     textRect.center = (pos[0], pos[1])
     screen.blit(text,textRect)
 
+
+
 while running:
     # poll for events
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        if event.type == pg.KEYDOWN:
+            # debug
+            if event.key == pg.K_a:
+                if GAMESTATE == 'jogo':
+                    GAMESTATE = 'gameover'
+                else:
+                    GAMESTATE = 'jogo'
+            if event.key == pg.K_d:
+                novo_jogo()
     
     # ------------- TICK -------------
+    if GAMESTATE == 'jogo':
+        pass
+    
+    elif GAMESTATE == 'gameover':
+        pass
 
     screen.fill((47,69,65))
     # ------------- RENDER -------------
-    #screen.blit(teste,(10,10))
-    mostrar_texto('teste', 73,list(pg.mouse.get_pos()))
+    if GAMESTATE == 'jogo':
+        mostrar_texto(resposta, 50, [300,500])
+        screen.blit(img_coracao, [10,10])
     
+    elif GAMESTATE == 'gameover':
+        mostrar_texto('gameover', 50, [300,50])
+    
+
     pg.display.flip()
     clock.tick(60)
 
